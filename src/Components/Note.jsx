911d68cecbca70c8ace1d20f5243ed150d31c3d0
Benito Lopez <bennobenno2730@gmail.com>
2023-08-04 21:26:26 -0600

    Creando Keeper Clone con React

---
+++ src/Components/Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../index.css"
+
+function Note(){
+
+    return <div className="note">
+    <h1>Title the note</h1>
+    <p>Content the note</p>
+    </div>
+}
+export default Note
