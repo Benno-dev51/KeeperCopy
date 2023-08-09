6beff476cf3522ec039896152d8a2e5bf3f39f6d
Benito Lopez <bennobenno2730@gmail.com>
2023-08-08 19:40:48 -0600

    agregando json de notas y renderizando dinamicamente con props

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,6 +4,6 @@ import "../index.css"
 
 function Footer(){
 const year =new Date().getFullYear()
-    return <footer> <p>Hecho con amor por benito lopez {year}</p></footer>
+    return <footer className="footer"> <p>Hecho con amor por benito lopez {year}</p></footer>
 }
 export default Footer
